6ac68453a3de47e007de3dc3f2e533c954f57506
agungbudisan <agungbudisantoso@student.uns.ac.id>
2024-07-02 15:01:47 +0700

    Update Styling for Detail Warga

--- resources/views/dashboard/warga/show.blade.php
+++ resources/views/dashboard/warga/show.blade.php
@@ -1,52 +1,50 @@
 @extends('dashboard.layouts.main')
 
 @section('container')
-    <section class="flex flex-col min-h-screen">
+    <section class="flex flex-col min-h-screen bg-gray-100">
         <div class="container mx-auto mt-8 px-4">
             <h4 class="text-2xl font-semibold text-black mb-6 text-left">Profile Warga</h4>
         </div>
         <div class="container mx-auto mt-8 px-4">
-            <div class="flex flex-col md:flex-row">
-                <div class="md:w-1/4 flex flex-col justify-center rounded-2xl">
-                    <img src="/storage/warga/{{ $warga->image }}" class="rounded-2xl">
+            <div class="bg-white rounded-2xl shadow-lg p-6 flex flex-col md:flex-row">
+                <div class="md:w-1/4 flex flex-col justify-center rounded-2xl overflow-hidden">
+                    <img src="/storage/warga/{{ $warga->image }}" class="rounded-2xl object-cover w-full h-full">
                 </div>
-                <div class="md:w-1/2 flex flex-col justify-between ml-6 mt-4 md:mt-0">
-                    <table class="table-fixed">
+                <div class="md:w-3/4 flex flex-col justify-between ml-6 mt-4 md:mt-0">
+                    <table class="table-auto w-full">
                         <tbody>
                             <tr>
-                                <td class="font-semibold">Nama</td>
-                                <td>{{ $warga->nama }}</td>
+                                <td class="font-semibold py-2 pr-4 w-40">Nama</td>
+                                <td class="py-2">: {{ $warga->nama }}</td>
                             </tr>
                             <tr>
-                                <td class="font-semibold">NIK</td>
-                                <td>{{ $warga->nik }}</td>
+                                <td class="font-semibold py-2 pr-4 w-40">NIK</td>
+                                <td class="py-2">: {{ $warga->nik }}</td>
                             </tr>
                             <tr>
-                                <td class="font-semibold">KK</td>
-                                <td>{{ $warga->kk }}</td>
+                                <td class="font-semibold py-2 pr-4 w-40">KK</td>
+                                <td class="py-2">: {{ $warga->kk }}</td>
                             </tr>
                             <tr>
-                                <td class="font-semibold">Jenis Kelamin</td>
-                                <td>{{ $warga->gender }}</td>
-
+                                <td class="font-semibold py-2 pr-4 w-40">Jenis Kelamin</td>
+                                <td class="py-2">: {{ $warga->gender }}</td>
                             </tr>
                             <tr>
-                                <td class="font-semibold">Agama</td>
-                                <td>{{ $warga->agama }}</td>
+                                <td class="font-semibold py-2 pr-4 w-40">Agama</td>
+                                <td class="py-2">: {{ $warga->agama }}</td>
                             </tr>
                             <tr>
-                                <td class="font-semibold">Tempat Lahir</td>
-                                <td>{{ $warga->tempatLahir }}</td>
+                                <td class="font-semibold py-2 pr-4 w-40">Tempat Lahir</td>
+                                <td class="py-2">: {{ $warga->tempatLahir }}</td>
                             </tr>
                             <tr>
-                                <td class="font-semibold">Tanggal Lahir</td>
-                                <td>{{ $warga->tanggalLahir }}</td>
+                                <td class="font-semibold py-2 pr-4 w-40">Tanggal Lahir</td>
+                                <td class="py-2">: {{ $warga->tanggalLahir }}</td>
                             </tr>
                             <tr>
-                                <td class="font-semibold">RT</td>
-                                <td>{{ $warga->rts->nama }}</td>
+                                <td class="font-semibold py-2 pr-4 w-40">RT</td>
+                                <td class="py-2">: {{ $warga->rts->nama }}</td>
                             </tr>
-
                         </tbody>
                     </table>
                 </div>
